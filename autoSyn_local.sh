37c61a6fb05f295e9d2332f673336dc6bab3f338
Chaojidahoufeng <13146012600@163.com>
2020-12-16 21:09:45 +0800

    add some code from Mac

--- autoSyn_local.sh
+++ autoSyn_local.sh
@@ -4,9 +4,4 @@ git add *
 git commit -m 'add some code from Mac'
 # git commit -m 'add some results from Server'
 git pull --rebase origin main   #domnload data
-<<<<<<< Updated upstream
 git push origin main           #upload data
-=======
-git push origin main            #upload data
->>>>>>> Stashed changes
-git stash pop
