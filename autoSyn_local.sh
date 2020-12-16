6e2555eb8af58e4a784a12467b980d6110b40580
Chaojidahoufeng <13146012600@163.com>
2020-12-16 21:11:15 +0800

    add some code from Mac

--- autoSyn_local.sh
+++ autoSyn_local.sh
@@ -4,4 +4,4 @@ git add *
 git commit -m 'add some code from Mac'
 # git commit -m 'add some results from Server'
 git pull --rebase origin main   #domnload data
-git push origin main           #upload data
+git push origin yuzi           #upload data
