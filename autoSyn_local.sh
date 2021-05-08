8959a8e43cc5baeeab725ab0a614664aada8befe
Yuzi Yan <yanyuzithu@163.com>
2021-05-08 08:49:45 +0800

    add some code from Mac

--- autoSyn_local.sh
+++ autoSyn_local.sh
@@ -3,5 +3,5 @@ git status
 git add *  
 git commit -m 'add some code from Mac'
 # git commit -m 'add some results from Server'
-#git pull --rebase origin yuzi   #domnload data
-git push origin yuzi           #upload data
+git pull --rebase origin yuzi   #domnload data
+git push        #upload data
