c8ff9a039aa5633f405b6c224611c4443be265fd
Yuzi Yan <yanyuzithu@163.com>
2021-05-10 09:33:43 +0800

    add some code from Mac

--- maddpg/experiments/run_rel_formation_only.sh
+++ maddpg/experiments/run_rel_formation_only.sh
@@ -1,5 +1,5 @@
 srun -p gpu -N 1 -n 1 \
 python -u train.py \
 --scenario rel_formation_only \
---episode-dir ../policy/model_maddpg_rel_formation_only.npy
+--episode-dir ../policy/model_maddpg_rel_formation_only.npy \
 --save-dir ../policy/model_maddpg_rel_formation_only.ckpt
