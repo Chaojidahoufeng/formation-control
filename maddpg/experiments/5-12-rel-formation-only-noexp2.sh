2f85a4bdd2fa602271e65e9e47219427ac8ae093
Yuzi Yan <yanyuzithu@163.com>
2021-05-12 20:13:12 +0800

    add some code from Mac

--- maddpg/experiments/5-12-rel-formation-only-noexp2.sh
+++ maddpg/experiments/5-12-rel-formation-only-noexp2.sh
@@ -2,8 +2,6 @@ srun -p gpu -N 1 -n 1 python -u train.py \
 --exp-name 5-12-rel-formation-only-avoid-5-form-0_05-dist-0_002
 --scenario rel_formation_only \
 --save-dir model_maddpg_rel_formation_only.ckpt \
---restore False \
---display False \
 --avoid-rew-weight 5 \
 --form-rew-weight 0.05 \
 --dist-rew-weight 0.002
